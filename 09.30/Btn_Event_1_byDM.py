448bbc03750d6ed1faa05500e47437a3dfc4ceb3
Degital-masochist <rlaeoaud29@gmail.com>
2024-09-30 19:13:29 +0900

    .

--- 09.30/Btn_Event_1_byDM.py
+++ 09.30/Btn_Event_1_byDM.py
@@ -29,23 +29,23 @@ class PushButtonApp:
 		self.monitor_thread = threading.Thread(target=self.monitor_gpio)
 		self.monitor_thread.start()
 		
-		def button_pressed_callback(self, channel):
-			self.button_count = +1
+	def button_pressed_callback(self, channel):
+			self.button_count += 1
 			self.master.after(0, self.update_gui)
 			
-		def update_gui(self):
+	def update_gui(self):
 			self.label.config(text="버튼 상태: 눌림")
 			self.count_label.config(text=f"버튼 누른 횟수: {self.button_count}")
 			self.master.after(200, self.reset_label)
 			
-		def reset_label(self):
+	def reset_label(self):
 			self.label.config(text="버튼상태: 대기중")
 			
-		def monitor_gpio(self):
+	def monitor_gpio(self):
 			while self.is_running:
 				self.master.update()
 				
-		def quit(self):
+	def quit(self):
 			self.is_running = False
 			GPIO.cleanup()
 			self.master.quit()
