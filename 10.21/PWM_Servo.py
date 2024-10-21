98eb33321ee4b5bd857e91a14ef28957ec1fdaad
Degital-Masochist <rlaeoaud29@gmail.com>
2024-10-21 15:49:54 +0900

    10.21

--- 10.21/PWM_Servo.py
+++ 10.21/PWM_Servo.py
@@ -13,9 +13,17 @@ try:
 	while True:
 		servo.ChangeDutyCycle(7.5)
 		time.sleep(1)
+		servo.ChangeDutyCycle(10.5)
+		time.sleep(1)
+		servo.ChangeDutyCycle(3.5)
+		time.sleep(1)
 		servo.ChangeDutyCycle(12.5)
 		time.sleep(1)
-		servo.ChangeDutyCycle(2.5)
+		servo.ChangeDutyCycle(5.5)
+		time.sleep(1)
+		servo.ChangeDutyCycle(9.5)
+		time.sleep(1)
+		servo.ChangeDutyCycle(4.5)
 		time.sleep(1)
 		
 except KeyboardInterrupt:
